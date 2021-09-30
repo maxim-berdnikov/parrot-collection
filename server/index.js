3ca9c0ce934312de2f23f6650be377bc2ebf1a2e
Maxim Berdnikov <36860922+m-berdnikov@users.noreply.github.com>
2021-09-30 16:38:57 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 // app.use("/api/comics", require("./routes/comics.routes"));
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
 
