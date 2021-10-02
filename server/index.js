e77ceaa4e177eb1aeedacbe9d6e6cee8f77e94fb
Maxim Berdnikov <m.radik25@gmail.com>
2021-10-02 14:53:34 +0300

    fix cors and key prop issues

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+var cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-app.use(express.json());
-
 app.use(express.json({ extended: true }));
 
+var corsOptions = {
+  // origin: 'http://localhost:3000/',
+  origin: 'https://m-berdnikov.github.io/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.get("/", (req, res) => {
-  res.send('hello world');
+  res.send("hello world");
 });
 
-app.use("/api/comics", require("./routes/comics.routes"));
+app.use("/api/comics", cors(corsOptions), require("./routes/comics.routes"));
 
 const PORT = process.env.PORT || 5000;
 
