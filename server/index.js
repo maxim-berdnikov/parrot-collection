88258bfc83f817d41da9708834e7393dbacc21b7
Maxim Berdnikov <m.radik25@gmail.com>
2021-10-02 16:19:53 +0300

    check cors

--- server/index.js
+++ server/index.js
@@ -8,17 +8,17 @@ require("dotenv").config();
 
 const cors = require("cors");
 
-// const corsOptions = {
-//     origin: ['https://m-berdnikov.github.io/'],
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
+const corsOptions = {
+    origin: ['https://m-berdnikov.github.io/'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-app.options("https://m-berdnikov.github.io/", cors());
+app.use(cors(corsOptions));
+// app.options("https://m-berdnikov.github.io/", cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
