9c76fa532a64606c66c2d55fe3c4e01da17be5cc
Maxim Berdnikov <m.radik25@gmail.com>
2021-10-02 16:16:47 +0300

    check cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ require("dotenv").config();
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.options("https://m-berdnikov.github.io/", cors());
 
 app.use(express.json({ limit: "50mb" }));
