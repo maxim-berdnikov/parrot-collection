afcf191be56d7c45686cd9937972a223ded19078
Maxim Berdnikov <m.radik25@gmail.com>
2024-08-11 19:34:46 +0300

    Поправить правила

--- client/src/Templates/Layout/Footer/index.tsx
+++ client/src/Templates/Layout/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Footer = (): JSX.Element => {
-	const year = new Date().getFullYear()
+	const year = new Date().getFullYear();
 	return (
 		<footer className="p-3 w-full bg-yellow-500 text-white font-bold">
 			@ {year} Parrot Collection
